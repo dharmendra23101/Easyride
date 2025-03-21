0e2521b1681966d6fc2b97320bed28f5b62b8a20
Dharmendra <dharmendra23101@gmail.com>
2025-03-22 00:34:23 +0530

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: "/Easyride/", // Ensure correct base path for GitHub Pages
-  plugins: [react()],
-});
